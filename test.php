7166ec2e9a76f0769981be40bab49226a97274fe
Tommy <meier@rhinos-media.de>
2016-02-03 15:35:03 +0100

    yes

--- test.php
+++ test.php
@@ -3,13 +3,33 @@
 require_once __DIR__ . '/string/Char.php';
 require_once __DIR__ . '/string/CharChain.php';
 
-require_once __DIR__ . '/cli/ParserInterface.php';
-require_once __DIR__ . '/cli/FullParser.php';
-require_once __DIR__ . '/cli/CliParser.php';
-require_once __DIR__ . '/cli/Arguments.php';
+use String\Char;
+use String\CharChain;
 
+$input = '-fTommy -l Meier -k=5 --fn="Tommy Meier" --age=17 --company=Rhinos\ Media --other= --flag';
+$expected = array(
+    '-fTommy',
+    '-l',
+    'Meier',
+    '-k=5',
+    '--fn=Tommy Meier',
+    '--age=17',
+    '--company=Rhinos Media',
+    '--other=',
+    '--flag'
+);
 
-$args   = new CLI\Arguments(CLI\Arguments::PARSER_CLI);
-$args->parse($argv);
+function parse($input)
+{
+    $chars = new CharChain($input);
+    $args  = array();
 
-var_dump($args->getArguments());
+    for($i = 0; $i < $chars->length() && $char = $chars[$i]; $i++)
+    {
+
+    }
+
+    return $args;
+}
+
+var_dump($expected == parse($input));
