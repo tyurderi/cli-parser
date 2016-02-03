d3d9bf2b31145b6b434d0437fccaa2050eb4dd42
Tommy <meier@rhinos-media.de>
2016-02-03 15:34:47 +0100

    yes

--- test.php
+++ test.php
@@ -3,11 +3,13 @@
 require_once __DIR__ . '/string/Char.php';
 require_once __DIR__ . '/string/CharChain.php';
 
-require_once __DIR__ . '/cli/Parser.php';
+require_once __DIR__ . '/cli/ParserInterface.php';
+require_once __DIR__ . '/cli/FullParser.php';
+require_once __DIR__ . '/cli/CliParser.php';
 require_once __DIR__ . '/cli/Arguments.php';
 
-$parser = new CLI\Parser;
-$args   = new CLI\Arguments($parser);
-$args->parse(implode(' ', array_slice($argv, 1)));
+
+$args   = new CLI\Arguments(CLI\Arguments::PARSER_CLI);
+$args->parse($argv);
 
 var_dump($args->getArguments());
