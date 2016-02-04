7e806ece2405056acb396a95faf4a117e7bdf148
Tommy <meier@rhinos-media.de>
2016-02-04 11:10:49 +0100

    add FullParser

---
+++ src/Cli/FullParser.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace CliParser\Cli;
+
+class FullParser
+{
+
+    /**
+     * @var PreParser
+     */
+    protected $preParser  = null;
+
+    /**
+     * @var PostParser
+     */
+    protected $postParser = null;
+
+    public function __construct()
+    {
+        $this->preParser  = new PreParser();
+        $this->postParser = new PostParser();
+    }
+
+    public function parse($input)
+    {
+        $result = $this->preParser->parse($input);
+        $result = $this->postParser->parse($result);
+
+        return $result;
+    }
+
+}
--- src/Cli/PreParser.php
+++ src/Cli/PreParser.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Cli;
+namespace CliParser\Cli;
 
 use String\Char;
 use String\CharChain;
