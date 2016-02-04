20a8c04d3a303aab776ca38105f0fc9d9808f484
Tommy <meier@rhinos-media.de>
2016-02-04 10:48:14 +0100

    initial commit

--- test.php
+++ test.php
@@ -1,23 +1,8 @@
 <?php
 
-require_once __DIR__ . '/string/Char.php';
-require_once __DIR__ . '/string/CharChain.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-require_once __DIR__ . '/cli/PreParser.php';
+$parser = new CliParser\Cli\PostParser();
+$result = $parser->parse(array_slice($argv, 1));
 
-$input = '-fTommy -l Meier -k=5 --fn="Tommy Meier" --age=17 --company=Rhinos\ Media --other= --flag';
-$expected = array(
-    '-fTommy',
-    '-l',
-    'Meier',
-    '-k=5',
-    '--fn=Tommy Meier',
-    '--age=17',
-    '--company=Rhinos Media',
-    '--other=',
-    '--flag'
-);
-
-$result = (new Cli\PreParser)->parse($input);
-
-var_dump($result, $expected == $result);
+var_dump($result);
